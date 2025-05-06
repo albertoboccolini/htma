85c75f7463922cd7de0619513299ea3fdd61a7c9
Alberto Boccolini <48589525+albertoboccolini@users.noreply.github.com>
2025-05-06 17:25:37 +0200

    Linted code (#1)

--- api/index.ts
+++ api/index.ts
@@ -1,26 +1,27 @@
-import express, { Request, Response } from "express";
-import TurndownService from "turndown";
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import express from 'express';
+import type {Request, Response} from 'express';
+import TurndownService from 'turndown';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const app = express();
 
 const turndownService = new TurndownService();
 
-app.get("/", async (req: Request, res: Response): Promise<any> => {
+app.get('/', async (req: Request, res: Response): Promise<any> => {
 	const url = req.query.url as string;
 
 	if (!url) {
-		return res.status(400).json({ error: "Missing URL parameter" });
+		return res.status(400).json({ error: 'Missing URL parameter' });
 	}
 
 	try {
 		const headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-			Accept: "text/html,application/xhtml+xml",
-			"Accept-Language": "it-IT,it;q=0.9",
-			Connection: "keep-alive",
-			Referer: "https://www.google.com/",
-			DNT: "1",
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+			Accept: 'text/html,application/xhtml+xml',
+			'Accept-Language': 'it-IT,it;q=0.9',
+			Connection: 'keep-alive',
+			Referer: 'https://www.google.com/',
+			DNT: '1',
 		};
 
 		const response = await fetch(url, { headers: headers });
@@ -29,8 +30,8 @@ app.get("/", async (req: Request, res: Response): Promise<any> => {
 		}
 
 		const html = await response.text();
-		turndownService.remove("script");
-		turndownService.remove("style");
+		turndownService.remove('script');
+		turndownService.remove('style');
 		const markdown = turndownService.turndown(html);
 
 		res.send({
@@ -38,12 +39,12 @@ app.get("/", async (req: Request, res: Response): Promise<any> => {
 		});
 	} catch (err: any) {
 		res.status(500).json({
-			error: "Error fetching or converting page",
+			error: 'Error fetching or converting page',
 			details: err.message,
 		});
 	}
 });
 
-export default (req: VercelRequest, res: VercelResponse) => {
+export default (req: VercelRequest, res: VercelResponse): void => {
 	app(req as any, res as any);
 };
